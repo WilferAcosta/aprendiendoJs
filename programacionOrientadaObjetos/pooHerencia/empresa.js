b9ab88748ce88b10c1c3a316a86b96825e13b7c9
WilferAcosta <w.o.a.r@hotmail.es>
2023-04-28 17:44:03 -0500

    html empresa

--- programacionOrientadaObjetos/pooHerencia/empresa.js
+++ programacionOrientadaObjetos/pooHerencia/empresa.js
@@ -1,9 +1,13 @@
+//clase Personas
 class Personas{
     #name
-    #identificacion
-    constructor(name,identificacion){
+    #edad
+    #ident
+    constructor(name,edad,identificacion){
         this.#name = name;
+        this.#edad = edad;
         this.#ident = identificacion;
+        
     };
     set name(nuevoName){
         this.#name = nuevoName;
@@ -17,31 +21,66 @@ class Personas{
     get ident(){
         return this.#ident;
     };
-
-}
-class Empleados extends Empresa{
-    #edad;
-    #salario
-    constructor(name,ident,edad,salario,Experiencia){
-        super(name,ident);
-        this.#edad=edad;
-        this.#salario=salario;
-    }
-    set edad(nuevaEdad){
-        this.#edad=nuevaEdad;
+    set edad(nuevaedad){
+        this.#edad= nuevaedad;
     }
     get edad(){
-        return this.#edad
+        return this.#edad;
+    }
+}
+// class abstracta de Personas
+class Empresa{
+    #nameE
+    #nit
+    constructor(nameE,nit,Personas){
+        this.#nameE = nameE;
+        this.#nit= nit;
+    }
+    set nameE(nuevoNameE){
+        this.#nameE = nuevoNameE;
+    }
+    get nameE(){
+        return this.#nameE;
+    }
+    set nit(nuevoNit){
+        this.#nit = nit;
+    }
+    get nit(){
+        return this.#nit;
+    }
+    incluir(){
+
+    };
+    modificar(){
+
+    };
+    eliminar(){
+
+    };
+    increSalario(){
+
+    };
+    sanciones(){
+
+    };
+}
+
+
+class Empleados extends Personas{
+    #salario
+    constructor(name,edad,ident,salario,Experiencia){
+        super(name,edad,ident);
+        this.#salario = salario;
     }
     set salario(nuevoSalario){
-        this.#salario=nuevoSalario;
+        this.#salario = nuevoSalario;
     }
     get salario(){
         return this.#salario;
-    }
+    }     
 }
 class Experiencia{
-    #descripcion
+    #descri
     #tiempo
     constructor(descripcion,tiempo){
         this.#descri=descripcion;
@@ -60,12 +99,28 @@ class Experiencia{
         return this.#tiempo;
     }
 }
-class Clientes extends Empresa{
+class Clientes extends Personas{
     #telefono
     #correo
     constructor(name,ident,telefono,correo){
-        super(name,ident);
+        super(name,edad,ident);
         this.#telefono = telefono;
         this.#correo = correo;
     }
+    set telefono(nuevoTel){
+        this.#telefono= nuevoTel;
+    }
+    get telefono(){
+        return this.#telefono;
+    }
+    set correo(nuevoCorreo){
+        this.#correo = nuevoCorreo;
+    }
+    get correo(){
+        return this.#correo;
+    }
 }
+let exp1 = new Experiencia("Programador","2 años");
+const emple1 = new Empleados("wilfer",28,11101693233,4_000_000,exp1);
+const empresa1= new Empresa("CAMPUS",123,emple1);
+console.log(empresa1);
