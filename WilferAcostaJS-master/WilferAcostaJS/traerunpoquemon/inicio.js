d0325a59372808b29e27d819b416e45ef534ee09
WilferAcosta <w.o.a.r@gmail.es>
2023-05-05 21:39:58 -0500

    05/05/2023

--- WilferAcostaJS-master/WilferAcostaJS/traerunpoquemon/inicio.js
+++ WilferAcostaJS-master/WilferAcostaJS/traerunpoquemon/inicio.js
@@ -1,46 +1,57 @@
-let id;
-
+let id1;
+let id2;
 function pokemon() {
-
     randomPokemon();
-
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-
-
-    fetch(url)
+    const url1 = `https://pokeapi.co/api/v2/pokemon/${id1}`;
+    const url2 = `https://pokeapi.co/api/v2/pokemon/${id2}`;
+    fetch(url1)
         .then(response => response.json())
         .then(data => {
-
-
             let pkmName = capitalize(data.name);
             let pkmweight = (data.weight);
             let pkmheight = (data.height);
-            
+            //creamos el titulo pokemos
             document.getElementById('pokemonMsg').innerHTML =`
             <h1 style="color: green;">Pokemon</h1>`
-
+            //agrego la imagen
             document.getElementById('pokemonSprite').src = data.sprites.front_default;
-            document.getElementById('pokemonQuantity').innerHTML
-
+            //agrego h1 con el nombre del pokemon
             document.getElementById('pokemonName').innerHTML = `
-             <h1 style="color: white;">${pkmName}</h1>`
-
-            
-
+            <h1 style="color: black;">${pkmName}</h1>`
+            //agrego el ancho del pochemon
             document.getElementById('pokemonweight').innerHTML = `
             <h1 style="color: black;">${pkmweight}</h1>`
-            
+            //agrego el alto del pokemon
             document.getElementById('pokemonheight').innerHTML = `
             <h1 style="color: black;">${pkmheight}</h1>`
+        })
+        //pokemos 2
+        fetch(url2)
+        .then(response => response.json())
+        .then(data => {
+            let pkmName = capitalize(data.name);
+            let pkmweight = (data.weight);
+            let pkmheight = (data.height);
+            document.getElementById('pokemonMsg2').innerHTML =`
+            <h1 style="color: green;">Pokemon</h1>`
 
-            console.log(pkmName);
-            console.log(pkmheight);
-            console.log(pkmweight);
+            document.getElementById('pokemonSprite2').src = data.sprites.front_default;
+
+            document.getElementById('pokemonName2').innerHTML = `
+            <h1 style="color: black;">${pkmName}</h1>`
+
+            document.getElementById('pokemonweight2').innerHTML = `
+            <h1 style="color: black;">${pkmweight}</h1>`
+            
+            document.getElementById('pokemonheight2').innerHTML = `
+            <h1 style="color: black;">${pkmheight}</h1>`
         })
 }
 
+
 function randomPokemon() {
-    id = Math.round(Math.random() * 150) + 1;
+    id1 = Math.round(Math.random() * 150) + 1;
+    id2 = Math.round(Math.random() * 150) + 1;
 }
 
 const capitalize = (s) => {
