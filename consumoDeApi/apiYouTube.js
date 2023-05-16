48706b93535e18ff8655775fc697bac99509cd31
WilferAcosta <w.o.a.r@hotmail.es>
2023-05-16 17:39:13 -0500

    16/05/2023

--- consumoDeApi/apiYouTube.js
+++ consumoDeApi/apiYouTube.js
@@ -14,19 +14,39 @@ try {
 	const response = await fetch(url, options);
 	const result = await response.json();
     console.log(result);
+	console.log();
 	const id = result.contents[1].video.videoId;
-    document.getElementById("video").innerHTML = `<iframe width="100%" height="500" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
-    result.forEach(e =>{
-		console.log(result.contents[e+2].video.videoId);
-		body += `<iframe width="100%" height="500" src="https://www.youtube.com/embed/${result.contents[e+2].video.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
-	});
-	document.getElementById("body").innerHTML += body ;
-    
+
+    document.getElementById("video").innerHTML = `<iframe width="100%" height="500" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+	<h1><img src="${result.contents[1].video.author.avatar[0].url}" alt="avatar">${result.contents[1].video.title}<span>(${result.contents[1].video.publishedTimeText}) vistas:${result.contents[1].video.stats.views}</span></h1><br>
+	<h2>Descripcion</h2>
+	<p>${result.contents[1].video.descriptionSnippet}</p>`;
+    comentarios(id);
+	document.getElementById("description").innerHTML = `<br><h2>Comentarios</h2><br><p></p>`;
 } catch (error) {
 	console.error(error);
 }
 }
-// async function descripVideo(id){
+async function comentarios(id){
+	console.log(id)
+	const url = `https://youtube138.p.rapidapi.com/video/comments/?id=${id}&hl=en&gl=US`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3c3a7a1fa4msh0e92c078fd61424p137867jsnebb20098d172',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
 
-// }
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
+	result.comments.forEach(e => {
+		document,getElementById("comentarios").innerHTML = `<div><img src="${e.author[e+1].url}"><p>${e.author[e+1].content}</p></div>`
+	});
+} catch (error) {
+	console.error(error);
+}
+}
 
