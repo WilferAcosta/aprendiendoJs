a3babded24ad5cb263511a3bb34334a9e512f291
WilferAcosta <w.o.a.r@hotmail.es>
2023-05-23 17:26:08 -0500

    UN DIA MAS PERDIDO

---
+++ programacionOrientadaObjetos/Lapiz.js
@@ -0,0 +1,58 @@
+/**creamos la clase padre  lapiz */
+class Lapiz{
+    color;
+    tamaño;
+    #material;
+    #mina;
+    /**
+     * 
+     * @param {color} *atributo color con el modificador de acceso publico 
+     * @param {tamaño} *atributo tamaño con el modificador de acceso public
+     * @param {material} *atributo material con el modificador de acceso privados
+     * @param {mina} *atributo mina con el modificador de acceso privados
+     * estos atributos los recibe el constructor como parametros;
+     */
+    constructor({color="amarilla", tamaño="9cm",material="plastico",mina="carbon"}){
+        this.color = color;
+        this.tamaño= tamaño;
+        this.#material = material;
+        this.#mina= mina;
+    };
+    /**
+     * se crea el encapsulamiento de las atributos privados . 
+     */
+    setMaterial(material){
+        this.#material = material;
+    }
+    getMaterial(){
+        return this.#material;
+    }
+    setMina(mina){
+        this.#mina = mina;
+    }
+    getMina(){
+        return this.#mina;
+    }
+    
+}
+/**
+ * creamos la instancia de la clase padre Lapiz.
+ */
+const lapiz1 = new Lapiz({tamaño: "10cm"});
+console.log(lapiz1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
