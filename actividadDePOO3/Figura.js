0aaf367c9eb0b224b1a49c25f5ff4add8bde2e1a
WilferAcosta <w.o.a.r@hotmail.es>
2023-05-24 21:57:03 -0500

    24/05/2023

--- actividadDePOO3/Figura.js
+++ actividadDePOO3/Figura.js
@@ -1,7 +1,7 @@
 class Figura{
     #color
     #area
-    constructor({color,area}){
+    constructor(color,area){
         this.#color = color;
         this.#area = area;
     }
@@ -21,10 +21,10 @@ class Figura{
         return a*b
     }
 }
-class circulor{
+class Circulor extends Figura{
     #radio
-    constructor({color,area,radio}){
-        super({color,area})
+    constructor(color,area,radio){
+        super(color,area)
         this.#radio = radio;
     }
     set setRadio(radio){
@@ -37,11 +37,11 @@ class circulor{
             return Math.PI * this.#radio * this.#radio;
         }
 }
-class rectangulo{
+class Rectangulo extends Figura{
     #largo
     #ancho
-    constructor({largo,ancho}){
-        super({color,area})
+    constructor(color,area,largo,ancho){
+        super(color,area)
         this.#largo = largo;
         this.#ancho = ancho;
     }
@@ -61,33 +61,52 @@ class rectangulo{
         return this.#largo * this.#ancho;
     }
 }
-
-document.addEventListener('click',(e)=>{
-    if(e.target.id=='sumit'){
-        enviar();
-    }
-})
-
-function enviar(){
-    let color= document.getElementById("color").value;
-    let area = document.getElementById("area").value;
-    let raza = document.getElementById("raza").value;
+document.addEventListener("DOMContentLoaded", function() {
+    let select = document.getElementById("area");
     let circulo = document.getElementById("circulo");
     let rectangulo = document.getElementById("rectangulo");
-    switch(area){
-        case "circulo":
-            circulo.style.display="block";
-
-            
-            break;
-        case "rectangulo":
-            break;
-    }
-
-    let perro1 = new Perro({nombre,edad,raza});
-    alert(perro1.moverCola());
-    alert(`El animal se llama ${perro1.getNombre}`);
-    
-}
-
-
+    let submitButton = document.getElementById("submit");
+  
+    select.addEventListener("change", function() {
+      let selectedOption = select.value;
+  
+      if (selectedOption === "circulo") {
+        circulo.style.display = "block";
+        rectangulo.style.display = "none";
+      } else if (selectedOption === "rectangulo") {
+        circulo.style.display = "none";
+        rectangulo.style.display = "block";
+      } else {
+        circulo.style.display = "none";
+        rectangulo.style.display = "none";
+      }
+    });
+  
+    submitButton.addEventListener("click", function(event) {
+      event.preventDefault();
+  
+      let color = document.getElementById("color").value;
+  
+      if (select.value === "circulo") {
+        let radio = document.getElementById("radio").value;
+        let circuloInstance = new Circulor(color, "Círculo", radio);
+        let area = circuloInstance.calcularArea();
+        console.log("Círculo seleccionado");
+        console.log("Radio: " + radio);
+        console.log("Color: " + color);
+        console.log("Área: " + area);
+        // Realiza acciones con la instancia de circuloInstance y el área calculada
+      } else if (select.value === "rectangulo") {
+        let largo = document.getElementById("largo").value;
+        let ancho = document.getElementById("ancho").value;
+        let rectanguloInstance = new Rectangulo(color, "Rectángulo", largo, ancho);
+        let area = rectanguloInstance.calcularArea();
+        console.log("Rectángulo seleccionado");
+        console.log("Largo: " + largo);
+        console.log("Ancho: " + ancho);
+        console.log("Color: " + color);
+        console.log("Área: " + area);
+        // Realiza acciones con la instancia de rectanguloInstance y el área calculada
+      }
+    });
+  });
